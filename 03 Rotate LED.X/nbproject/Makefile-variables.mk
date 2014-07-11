e6da570edb8db2a4872200aa564ae1ced8cc00a0
roberto <roberto@3fb5ac2b-7b2e-45e6-9c49-e85a37d3f286>
2014-07-11 03:14:41 +0000

    Terminado de subir cambios residentes del ejercicio 3.

---
+++ 03 Rotate LED.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=03_Rotate_LED.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/03_Rotate_LED.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=03rotateled.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/03rotateled.x.tar
